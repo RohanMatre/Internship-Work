9415442a9c60013ccc5b2e9545f53ac96fa1b5f1
Rohan Matre <100989394+RohanMatre@users.noreply.github.com>
2024-07-14 17:10:20 +0530

    Update sidemap.ts

--- src/app/sidemap.ts
+++ src/app/sidemap.ts
@@ -3,30 +3,42 @@ import { createClient } from "@/prismicio";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const client = createClient();
-  const homepage = await client.getSingle("homepage");
-  const pages = await client.getAllByType("page");
-  const blogPosts = await client.getAllByType("blog_post");
-  const projects = await client.getAllByType("project");
+  
+  // Fetch homepage data synchronously
+  const homepage = client.getSingle("homepage");
+  
+  // Fetch pages, blog posts, and projects asynchronously
+  const pagesPromise = client.getAllByType("page");
+  const blogPostsPromise = client.getAllByType("blog_post");
+  const projectsPromise = client.getAllByType("project");
+
+  // Await all promises concurrently
+  const [homepageData, pages, blogPosts, projects] = await Promise.all([
+    homepage,
+    pagesPromise,
+    blogPostsPromise,
+    projectsPromise,
+  ]);
 
   const siteRoot = "https://demo.com";
 
   const homepageRoute = {
     url: siteRoot,
-    lastModified: homepage.last_publication_date,
+    lastModified: homepageData.last_publication_date,
   };
 
   const pagesRoutes = pages.map((page) => ({
-    url: siteRoot + "/" + page.uid,
+    url: `${siteRoot}/${page.uid}`,
     lastModified: page.last_publication_date,
   }));
 
   const blogPostsRoutes = blogPosts.map((post) => ({
-    url: siteRoot + "/blog/" + post.uid,
+    url: `${siteRoot}/blog/${post.uid}`,
     lastModified: post.last_publication_date,
   }));
 
   const projectsRoutes = projects.map((project) => ({
-    url: siteRoot + "/project/" + project.uid,
+    url: `${siteRoot}/project/${project.uid}`,
     lastModified: project.last_publication_date,
   }));
 
